fcdec41096f7945fcbfabbe039d24cbad4ea68d1
alswp322 <citrusfish@naver.com>
2025-02-17 17:24:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const results = {
+  ISTJ: "<a href='https://ifh.cc/v-RjdOmo' target='_blank'><img src='https://ifh.cc/g/RjdOmo.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>클래식한 작업을 하는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 화이트<br>• 스팟: 블랙 x3<br><br>신뢰감있고 깔끔한 작품을 만드는 당신<br> 단정한 화이트 베이스 위에<br>블랙 스파클 세배를 더해 균형 있는 조합<br>정식 명칭은 달마시안 유약인 블랙&화이트 유약이 당신의 유약 ☕️🔲</span>",
+  ISFJ: "<a href='https://ifh.cc/v-myRVQ7' target='_blank'><img src='https://ifh.cc/g/myRVQ7.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>따뜻한 감성을 담아내는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 화이트<br>• 스팟: 핑크, 퍼플<br><br>포근한 감성이 작품에서 느껴지는 당신<br>마치 부드러운 꽃잎이 흩날리는 듯한 조합이 어울려요<br>부드럽고 깔끔한 화이트 베이스에<br>핑크와 퍼플 스팟 포인트가 당신의 유약 🌸</span>",
+  INFJ: "<a href='https://ifh.cc/v-LOzsy7' target='_blank'><img src='https://ifh.cc/g/LOzsy7.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>영감을 전하는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 퍼플<br>• 스팟: 오렌지<br><br>당신의 작품에서는 무언가 신비로움이 느껴져요<br>클리어 젤리 퍼플 베이스에<br>따뜻한 오렌지 포인트를 더해 <br>몽환적인 느낌을 연출되는 조합이 당신의 유약 🔮🍊</span>",
+  INTJ: "<a href='https://ifh.cc/v-GBKotk' target='_blank'><img src='https://ifh.cc/g/GBKotk.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>혁신을 설계하는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 네이비<br>• 스팟: 블랙, 화이트<br><br>작품에 디테일이 돋보이는 당신!<br>짙은 코발트 네이비 베이스에 블랙으로 깊이를 주고<br>화이트 스팟으로 하이라이트를 주는<br>조화로운 유약⚙️</span>",
+  ISTP: "<a href='https://ifh.cc/v-l9X91q' target='_blank'><img src='https://ifh.cc/g/l9X91q.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>기술에 능한 세라미스트</span><br><br><span class='result-detail'>• 베이스: 딥그린(오리베)<br><br>• 스팟: 블랙, 화이트<br><br>손기술이 뛰어나시군요!<br>기술을 돋보이게 할br>독특한 조합을 추천드립니다<br>기물의 쉐잎과 라인이 강조되는 유약 조합!<br> 전통 유약인 오리베의 진한 딥 그린에<br> 블랙과 화이트 스팟을 얹은 무드가 바로 당신의 유약 👷✍️</span>",
+  ISFP: "<a href='https://ifh.cc/v-OlN6ks' target='_blank'><img src='https://ifh.cc/g/OlN6ks.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>감성을 표현하는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 퍼플<br>• 스팟: 미스테리 믹스<br><br>감각적인 아티스트로군요!<br>어디서도 볼 수 없는 신비한 조합<br>반투명한 젤리 퍼플 베이스에<br>미스테리 믹스 스팟을 더한 유약이 어울려요 🎭✨</span>",
+  INFP: "<a href='https://ifh.cc/v-gXNztQ' target='_blank'><img src='https://ifh.cc/g/gXNztQ.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>감각과 상상을 조형하는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 스카이 블루<br>• 스팟: 화이트, 오렌지<br><br>자유로운 상상력을 가진 당신에게 딱!<br>맑은 하늘 같은 스카이 블루에 화이트와 오렌지 스팟을 톡톡 💭🌞</span>",
+  INTP: "<a href='https://ifh.cc/v-t0LMrx' target='_blank'><img src='https://ifh.cc/g/t0LMrx.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>철학을 담은 세라미스트</span><br><br><span class='result-detail'>• 베이스: 화이트<br>• 스팟: 블루, 블랙<br><br>작품에 본질을 담아내는 당신을 위한 조합!<br>차분한 화이트 베이스 위에 스카이 블루와 블랙 스팟이 어우러져<br>단정한 무드를 연출하는 유약 📖🖋️</span>",
+  ESTP: "<a href='https://ifh.cc/v-dYzGFq' target='_blank'><img src='https://ifh.cc/g/dYzGFq.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>모험을 즐기는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 화이트<br>• 스팟: 레드, 오렌지<br>생동감 넘치는 당신에게 어울리는 컬러 조합!<br>화이트 베이스에 레드와 오렌지 스팟을 더해 에너지를 가득 🔥🎢</span>",
+  ESFJ: "<a href='https://ifh.cc/v-vVz1Vm' target='_blank'><img src='https://ifh.cc/g/vVz1Vm.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>협력을 이끄는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 핑크<br>• 스팟: 화이트, 오렌지<br><br>사람을 위한 예술을 만드는 당신!<br>핑크 베이스에 화이트와 오렌지 스팟을 더해<br>따뜻하고 친근한 감성을 담은 조합🦩<br>부드럽고 오렌지 사탕이 생각나는 컬러</span>",
+  ENFJ: "<a href='https://ifh.cc/v-g28GCj' target='_blank'><img src='https://ifh.cc/g/g28GCj.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>영감을 주는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 스카이 블루<br>• 스팟: 핑크, 퍼플<br><br>다른 도예가들에게도 영감을 주는 당신!<br>유약에 스카이 블루 베이스와 핑크, 퍼플 스팟이 어우러지면<br>신비롭고 몽환적인 분위기를 연출할 수 있어요 🍇💧</span>",
+  ENTJ: "<a href='https://ifh.cc/v-67M7Vs' target='_blank'><img src='https://ifh.cc/g/67M7Vs.png' border='0' style='width:100px; display:block; margin:0 auto;'></a><br><span class='result-title'>건축적 조형미를 담는 세라미스트</span><br><br><span class='result-detail'>• 베이스: 네이비<br>• 스팟: 화이트<br><br>강렬한 존재감과 구조적인 조형미를 가진 작품!<br>깊은 네이비 베이스에 화이트 스팟이 어우러져<br>신뢰감 있는 조합을 완성합니다 🌌</span>"
+};
+
+function startTest() {
+  document.getElementById("start-page").style.display = "none";
+  document.getElementById("test-page").style.display = "flex";
+}
+
+function nextQuestion(currentId, nextId) {
+  document.getElementById(currentId).classList.remove("active");
+  document.getElementById(nextId).classList.add("active");
+}
+
+function calculateResult() {
+  const form = document.getElementById("testForm");
+  const formData = new FormData(form);
+  let scores = { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0 };
+  
+  for (let [key, value] of formData.entries()) {
+    scores[value]++;
+  }
+  
+  let mbti = "";
+  mbti += scores["E"] > scores["I"] ? "E" : "I";
+  mbti += scores["S"] > scores["N"] ? "S" : "N";
+  mbti += scores["T"] > scores["F"] ? "T" : "F";
+  mbti += scores["J"] > scores["P"] ? "J" : "P";
+  
+  // 결과 페이지 표시
+  document.getElementById("test-page").style.display = "none";
+  document.getElementById("result-page").style.display = "flex";
+  
+  // 각 결과 문자열 출력 (해당 MBTI 결과에 맞게 이미지 포함)
+  document.getElementById("result-text").innerHTML = results[mbti];
+  
+  // 결과 페이지 배경 변경 (기존 배경 초기화 후 적용)
+  let resultPage = document.getElementById("result-page");
+  resultPage.style.background = "none";
+  resultPage.style.backgroundImage = "url('https://ifh.cc/g/HYmp9D.png')";
+  resultPage.style.backgroundSize = "cover";
+  resultPage.style.backgroundPosition = "center";
+  resultPage.style.backgroundRepeat = "no-repeat";
+}
+
+function restartTest() {
+  location.reload();
+}
